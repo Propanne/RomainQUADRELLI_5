34c1b5bf209a8f9df058f76257339814005683fd
roqua <romain.quadrelli@smile.fr>
2022-04-04 13:04:40 +0200

    Add product fields on page

--- front/js/ProductFunctions.js
+++ front/js/ProductFunctions.js
@@ -9,7 +9,20 @@ let canape = new Product();
 async function main() {
   // Request API product with previously retrieved ID
   var productJSON = await GetProductFromAPI(productID);
+  // Set Product params
   canape = new Product(productJSON.colors, productJSON._id, productJSON.name, productJSON.price, productJSON.imageUrl, productJSON.description, productJSON.altTxt);
+
+  // Set canape image
+  document.querySelector(".item__img").innerHTML += `<img src="${canape.imageUrl}" alt="${canape.altTxt}">`;
+  // Set canape name
+  document.getElementById('title').innerHTML += canape.name;
+  // Set canape price
+  document.getElementById('price').innerHTML += canape.price;
+  // Set canape description
+  document.getElementById('description').innerHTML += canape.description;
+  // Set canape colors
+  canape.colors.forEach( color => document.getElementById('colors').innerHTML += `<option value="${color}">${color}</option>`);
+
 
 }
 
